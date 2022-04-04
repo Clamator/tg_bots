3bc0113acf68aa33aa1b3e9aa7b6407b00f7e7b1
Clamator <kalmah23@gmail.com>
2022-04-04 23:09:33 +0300

    4

--- new_studying_test_bot/create_bot.py
+++ new_studying_test_bot/create_bot.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 import os
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+storage = MemoryStorage()
 
 #bot = Bot(token=os.getenv('TOKEN'))
 bot = Bot(token='5102696891:AAGsKx2cLvXsXosamo6eRbqC_dEbaFj0Ri0')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
--- new_studying_test_bot/handlers/admin.py
+++ new_studying_test_bot/handlers/admin.py
@@ -1,1 +1,62 @@
 from new_studying_test_bot.create_bot import dp, bot
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram import types, Dispatcher
+
+class FSMAdmin(StatesGroup):
+    photo = State()
+    location_name = State()
+    description = State()
+    tour_price = State()
+
+# main handler to add tour
+#@dp.message_handler(commands='add_tour', state=None)
+async def cm_start(message: types.Message):
+    await FSMAdmin.photo.set()
+    await message.reply('upload a photo')
+
+# catch the 1st answer, write into the dict by id
+#@dp.message_handler(content_types=['photo'], state=FSMAdmin.photo)
+async def upload_photo(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['photo'] = message.photo[0].file_id
+    await FSMAdmin.next()
+    await message.reply('Enter location name')
+
+# catch the 2nd answer
+#dp.message_handler(state=FSMAdmin.location_name)
+async def set_loc_name(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['location_name'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Enter description')
+
+# catch description
+#dp.message_handler(state=FSMAdmin.description)
+async def set_description(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['description'] = message.text
+    await FSMAdmin.next()
+    await message.reply('Enter tour price ')
+
+# catch tour price
+#dp.message_handler(state=FSMAdmin.tour_price)
+async def set_price(message: types.Message, state: FSMContext):
+    async with state.proxy() as data:
+        data['tour_price'] = float(message.text)
+
+    async with state.proxy() as data:
+        await message.reply(str(data))
+
+    await state.finish()
+
+
+# reg handlers
+def register_handlers_admin(dp: Dispatcher):
+    dp.register_message_handler(cm_start, commands=['upload_photo'], state=None)
+    dp.register_message_handler(upload_photo, content_types=['photo'], state=FSMAdmin.photo)
+    dp.register_message_handler(set_loc_name, state=FSMAdmin.location_name)
+    dp.register_message_handler(set_description, state=FSMAdmin.description)
+    dp.register_message_handler(set_price, state=FSMAdmin.tour_price)
+
+

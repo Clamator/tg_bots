2ef1cc9e586592c4a7b7cf9e58bf2dfc043d9719
Clamator <kalmah23@gmail.com>
2022-04-04 19:40:11 +0300

    3r

--- new_studying_test_bot/handlers/client.py
+++ new_studying_test_bot/handlers/client.py
@@ -23,7 +23,12 @@ async def show_contacts(message: types.Message):
     await bot.send_message(message.from_user.id, 'Moscow, Red Square, 1, +79999999999, mon-fr, 8:00-20:00', disable_web_page_preview=True,
                            protect_content=True, allow_sending_without_reply=False)
 
+async def show_tours(message: types.Message):
+    await bot.send_message(message.from_user.id, 'no available tours yet', disable_web_page_preview=True,
+                           protect_content=True, allow_sending_without_reply=False)
+
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
     dp.register_message_handler(command_help, commands=['help'])
     dp.register_message_handler(show_contacts, commands=['contacts'])
+    dp.register_message_handler(show_tours, commands=['tours'])
--- new_studying_test_bot/keyboards/client_kb.py
+++ new_studying_test_bot/keyboards/client_kb.py
@@ -1,8 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 
-b1 = KeyboardButton('/start')
-b2 = KeyboardButton('/help')
-b3 = KeyboardButton('/contacts')
+b1 = KeyboardButton('/help')
+b2 = KeyboardButton('/contacts')
+b3 = KeyboardButton('/tours')
 
-kb_client = ReplyKeyboardMarkup()
-kb_client.add(b1).add(b2).add(b3)
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+#kb_client.add(b1).add(b2).insert(b3)
+kb_client.row(b1, b2, b3)

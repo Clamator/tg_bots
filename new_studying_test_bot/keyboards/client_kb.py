ad4ac38efaf121a8e72e22a3a7ca1637cc1cd806
Clamator <kalmah23@gmail.com>
2022-04-04 21:31:42 +0300

    3

--- new_studying_test_bot/keyboards/client_kb.py
+++ new_studying_test_bot/keyboards/client_kb.py
@@ -3,7 +3,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemo
 b1 = KeyboardButton('/help')
 b2 = KeyboardButton('/contacts')
 b3 = KeyboardButton('/tours')
+#b4 = KeyboardButton('/share number', request_contact=True)
+#b5 = KeyboardButton('/share location', request_location=True)
 
 kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
 #kb_client.add(b1).add(b2).insert(b3)
-kb_client.row(b1, b2, b3)
+kb_client.add(b1).add(b2).insert(b3)#.row(b4, b5)
